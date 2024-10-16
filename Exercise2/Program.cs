c123386fd27a847e0c0200bb4ed755d6eff22a1d
Hzadez <hzadez63@gmail.com>
2024-10-16 12:43:04 +0400

    Yeni

---
+++ Exercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % n == 0)
+                {
+                    Console.WriteLine($"{n} ededi sade ededdir");
+
+                }   
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i % n == 0) && (i % n == 1))
+                {
+                   Console.WriteLine($"{n} ededi murekkeb ededdir");
+
+                }
+            }
+        }
+    }
+}
